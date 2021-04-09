e88edca08ab88c1580a862bb9e2810e062223137
XuLinrui <504645946@qq.com>
2021-04-09 11:13:11 +0800

    fix: 修复 commands/Register 方法的入参名称问题

--- app/commands/command.go
+++ app/commands/command.go
@@ -7,12 +7,12 @@ import (
 )
 
 // Register 注册根命令的 flag 或子命令
-func Register(cmd *cobra.Command)  {
+func Register(rootCmd *cobra.Command)  {
 	// 注册根命令的 flag
 	// ...
 
 	// 注册子命令
-	cmd.AddCommand(&cobra.Command{
+	rootCmd.AddCommand(&cobra.Command{
 		Use: "test",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("test subcommand...")
