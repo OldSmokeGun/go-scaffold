56254abb7a1c8f6e9261ef4899e051073f6570ac
oldSmokeGun <504645946@qq.com>
2022-01-16 18:09:43 +0800

    feat: 新增临时脚本的运行方式

--- internal/app/cli/cli.go
+++ internal/app/cli/cli.go
@@ -3,18 +3,50 @@ package cli
 import (
 	"github.com/spf13/cobra"
 	"go-scaffold/internal/app/cli/command/greet"
+	"go-scaffold/internal/app/cli/command/script"
+	"go-scaffold/internal/app/cli/pkg/commandx"
 	"go-scaffold/internal/app/global"
 )
 
+// Setup 命令行应用初始化入口
 func Setup() error {
+	cli := commandx.NewCommandLine(global.Command())
+
+	// 注册业务的子命令
 	greetHandler := greet.NewHandler()
+	cli.RegistryBusiness([]*commandx.Command{
+		{
+			Entity: &cobra.Command{
+				Use:   "greet",
+				Short: "示例子命令",
+				Run:   greetHandler.Default,
+			},
+			OptionFunc: func(command *cobra.Command) {
+				command.Flags().StringP("example", "e", "foo", "示例 flag")
+			},
+			Children: []*commandx.Command{
+				{
+					Entity: &cobra.Command{
+						Use:   "to",
+						Short: "示例子命令",
+						Run:   greetHandler.To,
+					},
+				},
+			},
+		},
+	})
 
-	greetCommand := &cobra.Command{Use: "greet", Run: greetHandler.Default}
-	greetCommand.AddCommand(&cobra.Command{Use: "to", Run: greetHandler.To})
-
-	global.Command().AddCommand(
-		greetCommand,
-	)
+	// 注册临时脚本命令
+	s0000000000 := script.NewS0000000000()
+	cli.RegistryScript([]*commandx.Command{
+		{
+			Entity: &cobra.Command{
+				Use:   "S0000000000",
+				Short: "示例脚本 S0000000000",
+				Run:   s0000000000.Run,
+			},
+		},
+	})
 
 	return nil
 }
