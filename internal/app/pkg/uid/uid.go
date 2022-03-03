d57f386fec20c24a96749a40d204c67e41a440df
oldSmokeGun <504645946@qq.com>
2022-03-04 00:57:51 +0800

    perf: optimize the use of uid package

--- internal/app/pkg/uid/uid.go
+++ internal/app/pkg/uid/uid.go
@@ -6,35 +6,40 @@ import (
 	"time"
 )
 
-const nodeLimit = 1024
+var defaultRand = rand.New(rand.NewSource(time.Now().UnixNano()))
 
-type Uid struct {
+const maxNode = 1<<10 - 1
+
+type uid struct {
 	node int64
+	rand *rand.Rand
 }
 
-func New(options ...Option) *Uid {
-	uid := &Uid{}
-
-	for _, option := range options {
-		option(uid)
-	}
-
-	return uid
-}
-
-type Option func(uid *Uid)
+type Option func(uid *uid)
 
 func WithNode(node int64) Option {
-	return func(uid *Uid) {
+	return func(uid *uid) {
 		uid.node = node
 	}
 }
 
-func (u *Uid) Generate() (int64, error) {
+func WithRand(rand *rand.Rand) Option {
+	return func(uid *uid) {
+		uid.rand = rand
+	}
+}
+
+func Generate(options ...Option) (int64, error) {
+	u := &uid{rand: defaultRand}
+
+	for _, option := range options {
+		option(u)
+	}
+
 	var node int64
 
 	if u.node == 0 {
-		node = int64(rand.New(rand.NewSource(time.Now().UnixNano())).Intn(nodeLimit))
+		node = int64(u.rand.Intn(maxNode + 1))
 	} else {
 		node = u.node
 	}
