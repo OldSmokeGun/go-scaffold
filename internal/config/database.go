8330b7d96067f2d9164266e1b976afd21688e165
许林瑞 <30717069+OldSmokeGun@users.noreply.github.com>
2024-09-13 17:20:58 +0800

    fix: the postgres driver is not imported

--- internal/config/database.go
+++ internal/config/database.go
@@ -77,7 +77,7 @@ func (d DatabaseDriver) IsSupported() bool {
 
 const (
 	MySQL    DatabaseDriver = "mysql"
-	Postgres DatabaseDriver = "postgres"
+	Postgres DatabaseDriver = "pgx"
 	SQLite   DatabaseDriver = "sqlite3"
 )
 
