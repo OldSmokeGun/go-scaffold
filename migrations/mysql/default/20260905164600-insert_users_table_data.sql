182690cc5095588e546f34d4cad6adcb4ab1fe38
XuLinrui <504645946@qq.com>
2026-09-05 18:56:50 +0800

    fix(migrate): seed admin with MD5 password and mobile column

--- migrations/mysql/default/20260905164600-insert_users_table_data.sql
+++ migrations/mysql/default/20260905164600-insert_users_table_data.sql
@@ -2,8 +2,8 @@
 
 START TRANSACTION;
 
-INSERT INTO users (username, password, nickname, phone, salt, created_at, updated_at, deleted_at)
-VALUES ('admin', '2637a5c30af69a7bad877fdb65fbd78b', 'SuperAdmin', '13800138000', 'f521a082-de1c-4519-9829-a12ef133f02c', unix_timestamp(), unix_timestamp(), 0);
+INSERT INTO users (username, password, nickname, mobile, salt, created_at, updated_at, deleted_at)
+VALUES ('admin', MD5('Admin123!'), 'SuperAdmin', '', 'f521a082-de1c-4519-9829-a12ef133f02c', unix_timestamp(), unix_timestamp(), 0);
 
 COMMIT;
 
